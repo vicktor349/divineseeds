a34497cf76b7a00944a2ffba723ee71a850cd1b7
vicktoh <ayomidevictor349@gmail.com>
2022-10-13 19:00:27 +0100

    Heroku config

--- server/server.js
+++ server/server.js
@@ -2,7 +2,6 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const bodyParser = require('body-parser')
-const multer = require('multer')
 require('dotenv').config()
 const connectDB = require('./config/db')
 const cookieParser = require('cookie-parser')
@@ -11,24 +10,20 @@ const userRoute = require('./routes/userRoute')
 const studentRoute = require('./routes/studentRoute')
 
 
+
+
 connectDB()
 app.use(cors())
 app.use(express.json())
 app.use(cookieParser())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
-const fileStorage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, '../images')
-    },
-    filename: (req, file, cb) => {
-        cb(null, Date.now() + '--' + file.originalname)
-    }
-})
-const upload = multer({ storage: fileStorage }).single('passport')
-app.use('/api', upload, registerRoute)
+app.use('/api', registerRoute)
 app.use('/api', userRoute)
 app.use('/api', studentRoute)
+app.get('/', (req, res) => {
+    res.send('<h1>Successful</h1>')
+})
 
 
 
